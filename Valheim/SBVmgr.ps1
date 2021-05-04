2064045ac2a489867d105dd046b07a504aae6998
Hebgbs <bluufoxx@gmail.com>
2021-05-04 00:49:33 -0400

    Mod management and stuff

---
+++ Valheim/SBVmgr.ps1
@@ -0,0 +1,105 @@
+function mkLn {
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\assets" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\assets" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\BetterArchery" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\BetterArchery" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Compass" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\Compass" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\files" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\files" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\FishFood" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\FishFood" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Jowleth" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\Jowleth" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\MasterChef" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\MasterChef" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\MeadBaseIconFix" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\MeadBaseIconFix" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\PotionsPlus" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\PotionsPlus" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ValheimFPSBoost" -ItemType Junction -Path "$steamPath\modded\Valheim\BepInEx\plugins\ValheimFPSBoost" | Out-Null
+
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ABearCodes.Valheim.SimpleRecycling.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ABearCodes.Valheim.SimpleRecycling.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\AddAllFuel.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\AddAllFuel.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Advize_CartographySkill.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\Advize_CartographySkill.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\AnyPortal.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\AnyPortal.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\AutoSplitStack.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\AutoSplitStack.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\BetterLadders.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\BetterLadders.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\BetterWagon.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\BetterWagon.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ClockMod.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ClockMod.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Compass.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\Compass.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\CookingSkill.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\CookingSkill.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EmoteWheel.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EmoteWheel.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EquipmentAndQuickSlots.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EquipmentAndQuickSlots.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EquipWheel.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EquipWheel.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EquipWheelFour.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EquipWheelFour.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EquipWheelThree.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EquipWheelThree.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\EquipWheelTwo.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\EquipWheelTwo.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ExploreTogether.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ExploreTogether.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ExtendedItemDataFramework.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ExtendedItemDataFramework.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\FarmGrid.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\FarmGrid.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\FermenterStatus.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\FermenterStatus.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\FirstPersonValheimClientMod.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\FirstPersonValheimClientMod.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\HanticksRPChatMod1.0.0.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\HanticksRPChatMod1.0.0.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ImprovedBuildHud.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ImprovedBuildHud.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\InstantMonsterDrop.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\InstantMonsterDrop.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\itemdrawers.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\itemdrawers.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\JotunnLib.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\JotunnLib.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\LastUsedWeapons.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\LastUsedWeapons.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\MeadBaseIconFix.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\MeadBaseIconFix.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\MerchantWhereabouts.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\MerchantWhereabouts.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Moregates.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\Moregates.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\MultiCraft.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\MultiCraft.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Necromancy.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\Necromancy.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\NexusUpdate.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\NexusUpdate.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\NoStamCosts.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\NoStamCosts.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\OverheadAxeSwing.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\OverheadAxeSwing.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\QuickStack.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\QuickStack.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\RepairAll.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\RepairAll.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\SkillInjector.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\SkillInjector.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\Skills Give More Carry Weight.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\Skills Give More Carry Weight.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\skyheim.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\skyheim.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\ToastyTorches.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\ToastyTorches.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\TPWolves.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\TPWolves.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\TrashItems.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\TrashItems.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\UseEquipmentInWater.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\UseEquipmentInWater.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\UsefulPaths.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\UsefulPaths.dll" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\plugins\UsefulTrophies.dll" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\plugins\UsefulTrophies.dll" | Out-Null
+
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\advize.CartographySkill.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\advize.CartographySkill.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\aedenthorn.ClockMod.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\aedenthorn.ClockMod.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\aedenthorn.AutoSplitStack.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\aedenthorn.AutoSplitStack.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\aedenthorn.Compass.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\aedenthorn.Compass.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\aedenthorn.InstantMonsterDrop.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\aedenthorn.InstantMonsterDrop.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\bakaSpaceman.LastUsedWeapons.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\bakaSpaceman.LastUsedWeapons.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\Basil_NoStamCosts.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\Basil_NoStamCosts.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\BepIn.Sarcen.FarmGrid.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\BepIn.Sarcen.FarmGrid.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\com.github.abearcodes.valheim.simplerecycling.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\com.github.abearcodes.valheim.simplerecycling.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\com.lvh-it.valheim.useequipmentinwater.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\com.lvh-it.valheim.useequipmentinwater.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\com.pipakin.SkillInjectorMod.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\com.pipakin.SkillInjectorMod.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\com.rolopogo.plugins.exploretogether.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\com.rolopogo.plugins.exploretogether.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\com.urgemeuwu.betterwagon.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\com.urgemeuwu.betterwagon.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\flueno.MerchantWhereabouts.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\flueno.MerchantWhereabouts.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\gg.khairex.usefultrophies.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\gg.khairex.usefultrophies.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\ishid4.mods.betterarchery.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\ishid4.mods.betterarchery.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\maximods.valheim.multicraft.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\maximods.valheim.multicraft.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\Menthus.bepinex.plugins.UsefulPaths.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\Menthus.bepinex.plugins.UsefulPaths.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\mkz.itemdrawers.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\mkz.itemdrawers.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\Necromancy.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\Necromancy.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\org.bepinex.plugins.valheim.quick_stack.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\org.bepinex.plugins.valheim.quick_stack.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\org.bepinex.valheim.displayinfo.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\org.bepinex.valheim.displayinfo.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\org.davidwofford.plugins.meadbaseiconfix.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\org.davidwofford.plugins.meadbaseiconfix.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\randyknapp.mods.epicloot.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\randyknapp.mods.epicloot.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\randyknapp.mods.equipmentandquickslots.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\randyknapp.mods.equipmentandquickslots.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\randyknapp.mods.extendeditemdataframework.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\randyknapp.mods.extendeditemdataframework.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\randyknapp.mods.improvedbuildhud.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\randyknapp.mods.improvedbuildhud.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\RepairAll.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\RepairAll.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\rin_jugatla.AddAllFuel.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\rin_jugatla.AddAllFuel.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\SkillsGiveMoreCarryWeight.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\SkillsGiveMoreCarryWeight.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\SSyl.FishFood.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\SSyl.FishFood.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\SSyl.ToastyTorches.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\SSyl.ToastyTorches.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\thegreyham.valheim.CookingSkill.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\thegreyham.valheim.CookingSkill.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\Toawy.TPWolves.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\Toawy.TPWolves.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\uk.co.jowleth.valheim.orestatus.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\uk.co.jowleth.valheim.orestatus.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.emotewheel.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.emotewheel.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.equipwheel.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.equipwheel.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.equipwheelfour.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.equipwheelfour.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.equipwheelthree.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.equipwheelthree.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.equipwheeltwo.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.equipwheeltwo.cfg" | Out-Null
+  New-Item -Target "$repoPath\$repoBranch\BepInEx\config\virtuacode.valheim.trashitems.cfg" -ItemType SymbolicLink -Path "$steamPath\modded\Valheim\BepInEx\config\virtuacode.valheim.trashitems.cfg" | Out-Null
+}
+
+function modMgr {
+
+}
